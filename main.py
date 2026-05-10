7f9986100201dde4a1d22000a84518cb45541502
手話サロンなかよし <syuwasaronnakayosi@gmail.com>
2026-05-10 15:15:38 +0900

    first commit
    Commit to main

---
+++ main.py
@@ -0,0 +1,17 @@
+py-cord
+PyNaClimport os
+import discord
+from discord.ext import commands
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+bot.run(TOKEN)
